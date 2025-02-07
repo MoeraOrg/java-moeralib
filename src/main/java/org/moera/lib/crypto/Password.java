2419e9da6ab776a25544a03176e8383c8e961511
Shmuel Melamud <smelamud@redhat.com>
2025-02-07 13:49:31 +0200

    Move in UniversalLocation and Password utilities.

---
+++ src/main/java/org/moera/lib/crypto/Password.java
@@ -0,0 +1,40 @@
+package org.moera.lib.crypto;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import org.moera.lib.util.Util;
+
+public class Password {
+
+    public static String hash(String password) {
+        byte[] salt = new byte[Long.BYTES];
+        new SecureRandom().nextBytes(salt);
+        return hash(password, salt);
+    }
+
+    public static boolean validate(String hash, String password) {
+        byte[] data = Util.base64decode(hash);
+        byte[] salt = new byte[Long.BYTES];
+        System.arraycopy(data, data.length - salt.length, salt, 0, salt.length);
+        return hash(password, salt).equals(hash);
+    }
+
+    private static String hash(String password, byte[] salt) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(password.getBytes(StandardCharsets.UTF_8));
+            messageDigest.update(salt);
+            byte[] sha = messageDigest.digest();
+            byte[] result = new byte[sha.length + salt.length];
+            System.arraycopy(sha, 0, result, 0, sha.length);
+            System.arraycopy(salt, 0, result, sha.length, salt.length);
+            return Util.base64encode(result);
+        } catch (NoSuchAlgorithmException e) {
+            throw new CryptoException(e);
+        }
+    }
+
+}
