3e7737c4c01b542be94263f950c15f63b4ef5543
Shmuel Melamud <smelamud@redhat.com>
2025-02-11 21:04:41 +0200

    Rename CryptoUtil routines to the names similar to moeralib routines in other languages.

--- src/main/java/org/moera/lib/crypto/CryptoUtil.java
+++ src/main/java/org/moera/lib/crypto/CryptoUtil.java
@@ -128,7 +128,7 @@ public class CryptoUtil {
         }
     }
 
-    public static byte[] toRawPublicKey(ECPublicKey publicKey) {
+    public static byte[] rawPublicKey(ECPublicKey publicKey) {
         byte[] x = encodeUnsigned(publicKey.getW().getAffineX(), PUBLIC_KEY_LENGTH / 2);
         byte[] y = encodeUnsigned(publicKey.getW().getAffineY(), PUBLIC_KEY_LENGTH / 2);
         byte[] rawKey = new byte[x.length + y.length];
@@ -137,24 +137,12 @@ public class CryptoUtil {
         return rawKey;
     }
 
-    public static byte[] toUncompressedPublicKey(ECPublicKey publicKey) {
-        byte[] raw = toRawPublicKey(publicKey);
-        byte[] uncompressed = new byte[raw.length + 1];
-        uncompressed[0] = 0x04;
-        System.arraycopy(raw, 0, uncompressed, 1, raw.length);
-        return uncompressed;
-    }
-
-    public static ECPublicKey toPublicKey(byte[] rawKey) {
-        return toPublicKey(rawKey, EC_CURVE);
-    }
-
-    public static ECPublicKey toPublicKey(byte[] rawKey, String curve) {
+    public static ECPublicKey rawToPublicKey(byte[] rawKey) {
         byte[] x = new byte[PUBLIC_KEY_LENGTH / 2];
         byte[] y = new byte[PUBLIC_KEY_LENGTH / 2];
         System.arraycopy(rawKey, 0, x, 0, x.length);
         System.arraycopy(rawKey, x.length, y, 0, y.length);
-        ECParameterSpec parameterSpec = ECNamedCurveTable.getParameterSpec(curve);
+        ECParameterSpec parameterSpec = ECNamedCurveTable.getParameterSpec(EC_CURVE);
         ECPoint w = parameterSpec.getCurve().createPoint(decodeUnsigned(x), decodeUnsigned(y));
         ECPublicKeySpec keySpec = new ECPublicKeySpec(w, parameterSpec);
 
@@ -165,16 +153,12 @@ public class CryptoUtil {
         }
     }
 
-    public static byte[] toRawPrivateKey(ECPrivateKey privateKey) {
+    public static byte[] rawPrivateKey(ECPrivateKey privateKey) {
         return encodeUnsigned(privateKey.getS(), PRIVATE_KEY_LENGTH);
     }
 
-    public static ECPrivateKey toPrivateKey(byte[] rawKey) {
-        return toPrivateKey(rawKey, EC_CURVE);
-    }
-
-    public static ECPrivateKey toPrivateKey(byte[] rawKey, String curve) {
-        ECParameterSpec parameterSpec = ECNamedCurveTable.getParameterSpec(curve);
+    public static ECPrivateKey rawToPrivateKey(byte[] rawKey) {
+        ECParameterSpec parameterSpec = ECNamedCurveTable.getParameterSpec(EC_CURVE);
         ECPrivateKeySpec keySpec = new ECPrivateKeySpec(decodeUnsigned(rawKey), parameterSpec);
         try {
             return (ECPrivateKey) KeyFactory.getInstance("EC", "BC").generatePrivate(keySpec);
@@ -207,11 +191,13 @@ public class CryptoUtil {
         }
     }
 
-    public static RestoredFingerprint restore(byte[] bytes, FieldWithSchema[] schema) {
-        return restore(bytes, version -> schema);
+    public static RestoredFingerprint restoreFingerprint(byte[] bytes, FieldWithSchema[] schema) {
+        return restoreFingerprint(bytes, version -> schema);
     }
 
-    public static RestoredFingerprint restore(byte[] bytes, Function<Integer, FieldWithSchema[]> schemaProvider) {
+    public static RestoredFingerprint restoreFingerprint(
+        byte[] bytes, Function<Integer, FieldWithSchema[]> schemaProvider
+    ) {
         try (FingerprintReader reader = new FingerprintReader(bytes)) {
             Fingerprint fingerprint = reader.read(schemaProvider);
             return new RestoredFingerprint(fingerprint, reader.available());
@@ -232,7 +218,7 @@ public class CryptoUtil {
     }
 
     public static byte[] sign(byte[] fingerprint, byte[] privateKey) {
-        return sign(fingerprint, toPrivateKey(privateKey));
+        return sign(fingerprint, rawToPrivateKey(privateKey));
     }
 
     public static byte[] sign(byte[] fingerprint, ECPrivateKey privateKey) {
@@ -248,11 +234,11 @@ public class CryptoUtil {
         }
     }
 
-    public static boolean verify(byte[] fingerprint, byte[] signature, byte[] publicKey) {
-        return verify(fingerprint, signature, toPublicKey(publicKey));
+    public static boolean verifySignature(byte[] fingerprint, byte[] signature, byte[] publicKey) {
+        return verifySignature(fingerprint, signature, rawToPublicKey(publicKey));
     }
 
-    public static boolean verify(byte[] fingerprint, byte[] signature, ECPublicKey publicKey) {
+    public static boolean verifySignature(byte[] fingerprint, byte[] signature, ECPublicKey publicKey) {
         try {
             Signature sign = Signature.getInstance(Rules.SIGNATURE_ALGORITHM, "BC");
             sign.initVerify(publicKey);
