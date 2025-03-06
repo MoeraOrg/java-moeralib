9a48d8cf1abb3352d0066915e374107c96a52ca6
Shmuel Melamud <smelamud@redhat.com>
2025-03-07 01:16:14 +0200

    Remove 'field' from BodyMappingException.

--- src/main/java/org/moera/lib/node/types/body/BodyMappingException.java
+++ src/main/java/org/moera/lib/node/types/body/BodyMappingException.java
@@ -2,27 +2,12 @@ package org.moera.lib.node.types.body;
 
 public class BodyMappingException extends RuntimeException {
 
-    private String field;
-
     public BodyMappingException() {
         super("Error mapping a Body object");
     }
 
     public BodyMappingException(Throwable cause) {
         super("Error mapping a Body object", cause);
-    }
-
-    public String getField() {
-        return field;
-    }
-
-    public void setField(String field) {
-        this.field = field;
-    }
-
-    @Override
-    public String getMessage() {
-        return field != null ? super.getMessage() + ": " + field : super.getMessage();
     }
 
 }
